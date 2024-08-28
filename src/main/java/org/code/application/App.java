03f2ab027dd45f436648382b455ab6a989f2cdd0
AlvesLariany <info.larianyalves@gmail.com>
2024-08-27 22:37:29 -0300

    Tela de cadastro

--- src/main/java/org/code/application/App.java
+++ src/main/java/org/code/application/App.java
@@ -3,28 +3,26 @@ package org.code.application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
-/* Teste de exibição de interface */
 
 public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/gui/views/LoginView.fxml"));
-        HBox hBox = fxmlLoader.load();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/gui/views/RegisterView.fxml"));
+        VBox vbox = fxmlLoader.load();
 
-        Scene scene = new Scene(hBox);
+        Scene scene = new Scene(vbox);
 
-        scene.getStylesheets().add(getClass().getResource("/gui/styles/loginStyle.css").toExternalForm());
+        scene.getStylesheets().add(getClass().getResource("/gui/styles/RegisterStyle.css").toExternalForm());
 
         stage.setTitle("Login");
         stage.setScene(scene);
         stage.show();
     }
 
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
         launch();
     }
 }
